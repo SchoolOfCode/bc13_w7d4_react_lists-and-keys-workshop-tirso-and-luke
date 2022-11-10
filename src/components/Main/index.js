330030842bc906a87f3a681ae2c427b5d730a201
Tirsog <tirsog@gmail.com>
2022-11-10 16:38:34 +0000

    Completed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,11 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
+  return <main id="main">
+
+    {posts.map((post) => <Post key={post.postId} title={post.title} date={post.date} author={post.author} text={post.text} highlights={post.highlights} image={post.image} />)}
+    
+    </main>;
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post( props) {
+  return <article>
+    <h1>{props.title}</h1>
+    
+    <date>{props.date}</date>
+
+    <h2>{props.author}</h2>
+
+    <p>{props.text}</p>
+
+    {props.highlights.map((highlight) => <li key={highlight.indexOf()}>highlight</li>)}
+
+    <img src = {props.image.link} alt={props.image.alt}/>
+  </article>;
 }
 
 export default Post;
