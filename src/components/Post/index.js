86c9cc473630404cafda5f9b754e9db6821c73b5
luke123adams <lxa207@gmail.com>
2022-11-10 17:55:49 +0000

    fixed highlight

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './index.css';
 
-function Post( props) {
+function Post(props) {
   return <article>
     <h1>{props.title}</h1>
     
@@ -12,7 +12,7 @@ function Post( props) {
 
     <p>{props.text}</p>
 
-    {props.highlights.map((highlight) => <li key={highlight.indexOf()}>highlight</li>)}
+    {props.highlights.map((highlight) => <li key={highlight.indexOf()}>{highlight}</li>)}
 
     <img src = {props.image.link} alt={props.image.alt}/>
   </article>;
